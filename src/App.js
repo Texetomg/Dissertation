8342e737a241004f393711c235d6fa268ebea2fd
texetomg <texetomg@gmail.com>
2020-10-18 09:29:42 +0300

    Added less.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import css from './App.module.less'
 
 function App() {
   return (
-    <div className="App">
+    <div className={css.kek}>
       <header className="App-header">
+        12345
       </header>
     </div>
   );
